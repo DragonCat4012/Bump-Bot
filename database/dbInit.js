648463d9038f39e3626469baba49242de9bc605b
DragonCat || Kiara || Yumi <68660034+DragonCat4012@users.noreply.github.com>
2020-10-12 19:26:59 +0200

    First

---
+++ database/dbInit.js
@@ -0,0 +1,57 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const serversql = new Sequelize('server_table', 'user', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    logging: false,
+    storage: 'server.sqlite',
+});
+
+const Server = serversql.define('server_table', {
+    key: {
+        type: DataTypes.TEXT,
+    },
+    prefix: {
+        type: DataTypes.TEXT,
+        defaultValue: "#"
+    },
+    description: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+    },
+    color: {
+        type: DataTypes.TEXT,
+        defaultValue: 0
+    },
+    time: {
+        type: DataTypes.TEXT,
+        defaultValue: 0
+    },
+    channel: {
+        type: DataTypes.TEXT,
+        defaultValue: 0
+    },
+
+
+    partner: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+    ban: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+});
+
+
+const syncDatabase = async() => {
+    try {
+        await serversql.sync();
+        console.log(' > 🗸 Server Cache');
+    } catch (e) {
+        console.log(e);
+        process.exit(1);
+    }
+}
+
+module.exports = { Server, syncDatabase }
