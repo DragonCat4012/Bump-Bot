03773cce0da41e053abb3e31bb30ea8ba668143c
Kiara <frida.shivaa@gmail.com>
2020-12-28 21:22:18 +0100

    Update preview.js

--- commands/preview.js
+++ commands/preview.js
@@ -16,25 +16,17 @@ module.exports = {
      */
     async execute(msg, args) {
         const { colors, emotes } = msg.client;
-        let guild = await msg.client.database.server_cache.getGuild(msg.guild.id)
-        let badges = ""
-
-        if (guild.ban) badges += ":no_entry:  Banned! :no_entry:"
-        if (guild.partner) { badges += emotes.partner } else { badges += emotes.inactivpartner }
-        if (msg.guild.id == 553942677117337600) { badges += emotes.staff } else { badges += emotes.inactivstaff }
-        let des = guild.description != 0 ? guild.description : "None"
+        let guildData = await msg.client.database.server_cache.getGuild(msg.guild.id)
+        let des = guildData.description != 0 ? guildData.description : "None"
 
         let emb = rawEmb(msg)
             .setTitle(` Preview [${msg.guild.name}]`)
-            .setColor(guild.color != 0 ? guild.color : colors.info)
+            .setColor(guildData.color != 0 ? guildData.color : colors.info)
 
-
-        .setDescription(`${badges} \n **Description:**\n ${guild.description} 
+        .setDescription(`\n **Description:**\n ${des} 
     \n **Invite: [klick]** 
-    \n${emotes.owner} ${msg.guild.owner.user.tag} 
     \n :globe_with_meridians: ${msg.guild.region}
     \n ${emotes.user} ${msg.guild.memberCount}
-    \n ${emotes.bot} ${msg.guild.members.cache.filter(member => member.user.bot).size}
     `)
         return msg.channel.send(emb)
     }
